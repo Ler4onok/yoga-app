56f0490dea39098ea3f15f720d1d7f64f3456469
valeryia <valeryia@awesomedevelopers.eu>
2022-10-22 19:49:18 +0200

    pullstate added

--- client/src/App.js
+++ client/src/App.js
@@ -12,16 +12,13 @@ import {IntlProvider} from 'react-intl';
 import messages from './en.json';
 // mui
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
-import {AppStore} from './store/store';
 
 
 /**
  * Application root
  */
 function App() {
-    AppStore.useState(store => console.log(store))
     const routes = useRoutes();
-
 
     return (
         <IntlProvider messages={messages} locale='en'>
